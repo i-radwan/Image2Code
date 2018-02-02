df6e20c201ab5e5934dab6071046f7544d06a07b
Ibrahim Radwan <i.radwan1996@gmail.com>
2018-02-02 13:45:46 +0200

    Final GUI changes

--- gui/output/output.cpp
+++ gui/output/output.cpp
@@ -1,15 +1,26 @@
+#include <stdio.h>
 #include <iostream>
 using namespace std;
-bool isprime(int n) {
-if(n<2) return false;
-for(int i= 2; i*i<=n; ++i)
-if (n%i== 0)
-return false;
-return true;
+int a, b, c, d, mx, md, mn;
+int main() {
+while (cin >> a >> b >> c >> d) {
+
+mx = 2 * a;
+
+md = 2 * b;
+
+mn = c < d ? c : d;
+
+mn *= 2;
+
+if (d > mn || c > mn || 2*d >= mx || 2*d >= md) {
+cout << "-1" << endl;
 }
-int main () {
-int x;
-cin >>x ;
-cout << isprime(x);
+else {
+cout << mx << endl << md << endl << mn << endl;
+}
+}
+
 return 0;
+
 }
